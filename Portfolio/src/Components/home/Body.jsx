a1d751a1b4a34de3c19f228f9b608a98dcc05aae
Suraj Thapa <surajthapart@gmail.com>
2024-07-10 15:59:50 +0545

    Adding fancy styling to look cool

--- Portfolio/src/Components/home/Body.jsx
+++ Portfolio/src/Components/home/Body.jsx
@@ -1,34 +1,48 @@
+import "./body.css"
+import "./body2.css"
 export const Body = () => {
   return (
     <div className="body-start">
         <div className="body-left">
-            <div className="body-one">
-                <div className="body-left-one">
-                    <span>20+</span>
-                    <p>Personal Projects</p>
+            <div className="body-one ">
+                <div className="package">
+                    <div className="body-left-one package2">
+                        <span>20+</span>
+                        <p>Personal Projects</p>
+                    </div>
+                    
                 </div>
-                <div className="body-left-two">
-                    <p>Frontend</p>
-                    <p>Developer</p>
+                <div className="card">
+                    <div className="body-left-two card2">
+                        <p>Frontend</p>
+                        <p>Developer</p>
+                    </div>
+                    
                 </div>
             </div>
             <div className="body-two">
-                <div className="body-left-three">
-                    <p><span>20+</span></p>
-                    <p>Satisfied Client</p>
+                <div className="card">
+                    <div className="body-left-three card2">
+                        <p><span>20+</span></p>
+                        <p>Satisfied Client</p>
+                    </div>
+                   
                 </div>
-                <div className="body-left-four">
-                    <p><span>1</span>Years</p>
-                    <p id="op">Working as frontend</p>
+                <div className="package">
+                    <div className="body-left-four package2">
+                        <p><span>1</span>Years</p>
+                        <p id="op">Working as frontend</p>
+                    </div>
+                    
                 </div>
             </div>
         </div>
 
-        <div className="body-right">
-            <span>Your ultimate Frontend partner</span>
+        <div className="cards">
+            <p className="heading">Your ultimate Frontend partner</p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nostrum, enim mollitia ad perferendis quos, delectus, impedit sed veritatis tempore eveniet eaque soluta odit optio alias? Maxime facere consequuntur obcaecati!
              </p>
-             <a href="">Get in Touch &gt;</a>
+             <p><a href="">Get in Touch &gt;</a></p>
         </div>
     </div>
   )
