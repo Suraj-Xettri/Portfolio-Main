d6de51ffea05588585bf35e48c7dc358cc1db3ef
Suraj Thapa <surajthapart@gmail.com>
2024-07-11 14:22:28 +0545

    Just completed all the design for about us page

--- Portfolio/src/Components/about/About.jsx
+++ Portfolio/src/Components/about/About.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import "./about.css"
 
 const About = () => {
   return (
     <div className='except bg-black h-screen gap-14 overflow-hidden flex flex-col items-center pt-6'>
       <div className='flex gap-9 p-3  items-center justify-center flex-col w-full'>
         <h1 className='font-black text-white text-5xl'>About Me</h1>
-        <div className='w-8 h-2 bg-purple-600 rounded-2xl mt-[-20px]'>
+        <div className='w-8 h-2 color rounded-2xl mt-[-20px]'>
       </div>
 
       <div className='flex p-5 gap-10'>
-        <div className=' w-[250px] h-[320px] bg-blue-300 rounded-md'>
+        <div className=' w-[250px] h-[320px] imgCard rounded-md cursor-pointer'>
           <img src="" alt="" />
         </div>
 
@@ -21,21 +22,21 @@ const About = () => {
            <div className='flex flex-col gap-5'>
             <div className='flex items-center gap-6'>
               <span className='w-[100px] cursor-pointer'>HTML & CSS</span>
-              <div className='w-[300px] h-3 rounded-3xl bg-yellow-300'></div>
+              <div className='color w-[300px] h-3 rounded-3xl'></div>
             </div>
             <div className='flex items-center gap-6'>
               <span className='w-[100px] cursor-pointer'>Java Script</span>
-              <div className='w-[400px] h-3 rounded-3xl bg-yellow-300'></div>
+              <div className='color w-[400px] h-3 rounded-3xl'></div>
             </div>
 
             <div className='flex items-center gap-6'>
               <span className='w-[100px] cursor-pointer'>React</span>
-              <div className='w-[450px] h-3 rounded-3xl bg-yellow-300'></div>
+              <div className='color w-[450px] h-3 rounded-3xl'></div>
             </div>
 
             <div className='flex items-center gap-6'>
               <span className='w-[100px] cursor-pointer'>Tailwind CSS</span>
-              <div className='w-[300px] h-3 rounded-3xl bg-yellow-300'></div>
+              <div className='color w-[300px] h-3 rounded-3xl'></div>
             </div>
           </div>
         </div>
@@ -43,17 +44,17 @@ const About = () => {
 
       <div className='flex text-white justify-around items-center pt-10 w-full'>
           <div className="flex-1 flex justify-center items-center flex-col border-r p-5">
-            <p><span>1</span>Years</p>
+            <p className='text-2xl text-design'><span>1+</span></p>
             <p id="op">Working as frontend</p>
           </div>
 
           <div className="flex-1 flex justify-center items-center flex-col border-r p-5">
-            <p>20+</p>
+            <p className='text-2xl text-design'>20+</p>
             <p id="op">Completed Projects</p>
           </div>
 
           <div className="flex-1 flex justify-center items-center flex-col p-5">
-            <p><span>20+</span></p>
+            <p className='text-2xl text-design'><span>20+</span></p>
             <p>Satisfied Client</p>
           </div>
         </div>
